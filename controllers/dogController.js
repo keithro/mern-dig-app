abac304a35c036b75afa3f1e8d224603b518fad8
alyshia bentley <alyshiabentley@gmail.com>
2021-11-17 13:31:53 -0800

    finish backend

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Dog = require('../models/Dog');
 
-router.post("/", (req, res) => {
-  Dog.create(req.body)
-    .then(dog => res.status(201).json({ status: 201, data: dog }))
-    .catch((e) => console.log(e));
+router.post('/', (req, res) => {
+	Dog.create(req.body)
+		.then((dog) => res.status(201).json({ status: 201, data: dog }))
+		.catch((e) => console.log(e));
 });
+
+//get
+router.get('/', (req, res) => {
+	Dog.find().then((dog) => res.status(200).json({ status: 200, data: dog }));
+});
+
+//update
+
+router.put('/:id', (req, res) => {
+	Dog.findByIdAndUpdate(req.params.id, req.body, {new : true}).then(dog => res.status(200).json({status: 200, data: dog}));
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+  Dog.findByIdAndDelete(req.params.id).then(dog => res.status(204).send({msg: "deleted"}))
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const dogControllers = require('./controllers/dogController');
 PORT=process.env.PORT;
 
 app.use(cors());
-// Parses incoming URL-encoded data:
+
 app.use(express.urlencoded({ extended: false }));
-// Parses incoming JSON-encoded data:
+
 app.use(express.json());
 app.use(morgan("combined"));
 
